4b112d5f98ea7c7715f382d0dcfda4ccb34f3479
Andy Song <keechi@qq.com>
2014-04-23 10:21:45 +0800

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,9 @@
+function addClass(el, cls) {
+  var reg = new RegExp("(\\s|^)" + cls + "(\\s|$)");
+  if (!reg.test(el.className)) {
+    el.className += " " + cls;
+  }
+}
 function removeClass(el, cls) {
   var reg = new RegExp("(\\s|^)" + cls + "(\\s|$)");
   el.className = el.className.replace(reg, " ").replace(/(^\s*)|(\s*$)/g,"");
@@ -5,3 +11,14 @@ function removeClass(el, cls) {
 function hasClass(el, cls) {
   return el.className && new RegExp("(\\s|^)" + cls + "(\\s|$)").test(el.className);
 }
+
+// or use classList
+
+// Adding a class
+element.classList.add("bar");
+// Removing a class
+element.classList.remove("foo");
+// Checking if has a class
+element.classList.contains("foo");
+// Toggle a class
+element.classList.toggle("active");
