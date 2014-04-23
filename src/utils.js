3695bd4190511bbecb7f299f2036aca8c7283bdc
Andy Song <keechi@qq.com>
2014-04-23 12:39:15 +0800

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,8 @@
+/*
+referers : https://github.com/cubiq/iscroll/blob/master/src/utils.js
+*/
+
+
 function addClass(el, cls) {
   var reg = new RegExp("(\\s|^)" + cls + "(\\s|$)");
   if (!reg.test(el.className)) {
@@ -22,3 +27,25 @@ element.classList.remove("foo");
 element.classList.contains("foo");
 // Toggle a class
 element.classList.toggle("active");
+
+
+var _elementStyle = document.createElement('div').style;
+var _vendor = (function () {
+	var vendors = ['t', 'webkitT', 'MozT', 'msT', 'OT'],
+		transform,
+		i = 0,
+		l = vendors.length;
+
+	for ( ; i < l; i++ ) {
+		transform = vendors[i] + 'ransform';
+		if ( transform in _elementStyle ) return vendors[i].substr(0, vendors[i].length-1);
+	}
+
+	return false;
+})();
+
+function _prefixStyle (style) {
+	if ( _vendor === false ) return false;
+	if ( _vendor === '' ) return style;
+	return _vendor + style.charAt(0).toUpperCase() + style.substr(1);
+}
