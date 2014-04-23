017e3c0af6ff4e1ea6e4feebbd97262ed98e9dd4
Andy Song <keechi@qq.com>
2014-04-23 11:01:26 +0800

    Update slider.js

--- src/slider.js
+++ src/slider.js
@@ -1,5 +1,26 @@
+/**
+ * <div class="lslider">
+	<div class="lslider-images">
+		<a href=""><img src="" alt=""></a>
+		<a href=""><img src="" alt=""></a>
+		<a href=""><img src="" alt=""></a>
+	</div>
+
+	<div class="lsider-title"></div>
+	<div class="lslider-indicator">
+		<span class="lactvie"></span>
+		<span></span>
+		<span></span>
+	</div>
+</div>
+*/
+
+
 function Slider(id) {
   
   this.container = document.getElementById(id);
+  this.imagesbox = this.container.querySelector('.lslider-images');
+  this.titlebox = this.container.querySelector('.lslider-title');
+  this.indicatorbox = this.container.querySelector('.lslider-indicator');
   
 }
